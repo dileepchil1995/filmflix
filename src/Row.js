6e917be5b0237c08f6c82ea8782915b9c5614fde
dileepchil1995 <dilip.chilakapati1995@gmail.com>
2020-09-07 17:18:44 +1000

    Updated

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Row.css';
 
+
+
 function Row() {
     return (
         <div className="row">
@@ -17,9 +19,9 @@ function Row() {
                                 <h2>Prayanam (2020)</h2>
                                     <img 
                                         
-                                        className="row_poster"
-                                        src="https://firebasestorage.googleapis.com/v0/b/soda-films.appspot.com/o/images%2FScreen%20Shot%202020-09-02%20at%2011.47.51%20am.png?alt=media&token=5c5fb266-4d1f-4bb4-95a3-33f11f9e1c03" 
-                                        alt="" 
+                                    className="row_poster"
+                                    src="https://firebasestorage.googleapis.com/v0/b/soda-films.appspot.com/o/images%2FScreen%20Shot%202020-09-02%20at%2011.47.51%20am.png?alt=media&token=5c5fb266-4d1f-4bb4-95a3-33f11f9e1c03" 
+                                    alt="" 
                                         
                                     />
                                 </div>
@@ -50,6 +52,60 @@ function Row() {
                                         alt="" 
                                     />
                                 </div>
+                                <div>
+                                    <h2>The Cricket - A Micro -Thriller</h2>
+                                    <img 
+                                        
+                                        className="row_poster"
+                                        src="https://firebasestorage.googleapis.com/v0/b/soda-films.appspot.com/o/images%2FScreen%20Shot%202020-09-07%20at%2010.26.42%20am.png?alt=media&token=a9e381eb-fa7d-4147-91e5-7f870a45ef09" 
+                                        alt="" 
+                                    />
+                                </div>
+                                <div>
+                                    <h2>JAN 26 - The Film</h2>
+                                    <img 
+                                        
+                                        className="row_poster"
+                                        src="https://firebasestorage.googleapis.com/v0/b/soda-films.appspot.com/o/images%2FScreen%20Shot%202020-09-07%20at%2010.29.10%20am.png?alt=media&token=84fb97bf-5bab-4e76-b1ce-2f119b2fe0e1" 
+                                        alt="" 
+                                    />
+                                </div>
+                                <div>
+                                    <h2>Kala - The Film</h2>
+                                    <img 
+                                        
+                                        className="row_poster"
+                                        src="https://firebasestorage.googleapis.com/v0/b/soda-films.appspot.com/o/images%2FScreen%20Shot%202020-09-07%20at%2010.33.50%20am.png?alt=media&token=8be230dd-bdad-468f-82f9-76d8b689fee8" 
+                                        alt="" 
+                                    />
+                                </div>
+                                <div>
+                                    <h2>VICTORY - The Film</h2>
+                                    <img 
+                                        
+                                        className="row_poster"
+                                        src="https://firebasestorage.googleapis.com/v0/b/soda-films.appspot.com/o/images%2FScreen%20Shot%202020-09-07%20at%2010.35.47%20am.png?alt=media&token=d87d524c-f4c9-445c-b7d4-4f3b9a9beb29" 
+                                        alt="" 
+                                    />
+                                </div>
+                                <div>
+                                    <h2>THE CHOICE</h2>
+                                    <img 
+                                        
+                                        className="row_poster"
+                                        src="https://firebasestorage.googleapis.com/v0/b/soda-films.appspot.com/o/images%2FScreen%20Shot%202020-09-07%20at%2010.39.49%20am.png?alt=media&token=ffe15517-766c-4c65-9e0c-074b6902c8b5" 
+                                        alt="" 
+                                    />
+                                </div>
+                                <div>
+                                    <h2>MY ATTEMPT TO STAY BLANK</h2>
+                                    <img 
+                                        
+                                        className="row_poster"
+                                        src="https://firebasestorage.googleapis.com/v0/b/soda-films.appspot.com/o/images%2FScreen%20Shot%202020-09-07%20at%2010.42.13%20am.png?alt=media&token=eb29cf47-7b4b-474f-9a79-2e2bf61428b3" 
+                                        alt="" 
+                                    />
+                                </div>
                         
                         </div>
             </div> 
@@ -62,12 +118,7 @@ function Row() {
                         <div className="row_posters">
                                 <div>
                                     <h2></h2>
-                                    <img 
-                                        
-                                        className="row_poster"
-                                        src="" 
-                                        alt="" 
-                                    />
+                                    
                                 </div>
                             
                                 
